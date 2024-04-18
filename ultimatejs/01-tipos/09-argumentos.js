3be439a9e73a29a6659f25be3a18a51456404525
Carlos Romero <67611257+chinokoreano@users.noreply.github.com>
2024-04-18 18:56:16 -0500

    Clases

--- ultimatejs/01-tipos/09-argumentos.js
+++ ultimatejs/01-tipos/09-argumentos.js
@@ -1,7 +1,10 @@
-function suma(a){
-    return a + 2
+function suma(a, b){
+    console.log(arguments);
+    return a + b
 }
 
-let resultado = suma(1000);
+let resultado = suma(1000,500);
 console.log(resultado);
-console.log(suma(10));
+console.log(suma(10,5,8,8,9,10));
+
+console.log(typeof suma);
